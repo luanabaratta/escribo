82ee7abad3f6184432cdb073c619573937a6cbcd
Luana B. dos Santos <luanabaratta@Luanas-MacBook-Air.local>
2023-12-07 16:08:34 -0300

    iterating numbers

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const adder = referenceNumber => {
 
     let sum = 0;
+
+    for(let number = 0; number < referenceNumber; number++) {
+        if(number % 3 === 0 || number % 5 === 0) {
+            sum += number;
+        }
+    }
+
+    return sum;
 }
+
+console.log(adder(10));
+console.log(adder(11));
